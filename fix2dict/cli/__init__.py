62a4b2523eb778ed187de79701307304f0eaf295
Filippo Costa <filippocosta.italy@gmail.com>
2020-08-23 01:47:50 +0200

    Inflate and deflate

--- fix2dict/cli/__init__.py
+++ fix2dict/cli/__init__.py
@@ -1,8 +1,10 @@
 from .main import cli  # NOQA
 
 from .schema import schema  # NOQA
+from .deflate import deflate  # NOQA
 from .ep import ep  # NOQA
-from .filter_patch import filter_patch
+from .filter_patch import filter_patch  # NOQA
+from .inflate import inflate  # NOQA
 from .patch import patch  # NOQA
 from .repo_b import repo_b  # NOQA
 from .repo_o import repo_o  # NOQA
---
+++ fix2dict/cli/deflate.py
@@ -0,0 +1,21 @@
+import click
+
+from . import cli
+from .utils.xml import read_xml_root
+from .utils.json import beautify_json
+from ..extension_pack import ExtensionPack
+
+
+@cli.command()
+@click.argument("src", nargs=1, type=click.Path(exists=True))
+def deflate(src):
+    """
+    Transform an XML-formatted EP file into a JSON Patch.
+
+    <SRC> must be a valid XML file containing the definition of an Extension
+    Pack. The resulting JSON Patch will be printed.
+    """
+    root = read_xml_root("", src, opt=False)[0]
+    patch = ExtensionPack(root).to_jsonpatch().to_string()
+    print(beautify_json(patch))
+
